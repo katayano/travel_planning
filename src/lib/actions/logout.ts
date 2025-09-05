77012c6673811fd733d7d15a97a76d8e154646f3
katayama <idnlnroocwetn22@gmail.com>
2025-09-06 00:46:43 +0900

    Headerコンポーネントを作成

---
+++ src/lib/actions/logout.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { signOut } from "@/auth";
+
+/**
+ * ログアウト処理を行うServer Action
+ * NextAuth.jsのsignOut関数でセッションを削除し、ログインページにリダイレクト
+ * 
+ * @returns ログアウト処理の実行
+ */
+export async function logout(): Promise<void> {
+    // NextAuth.jsのsignOut関数でセッションを削除
+    // redirectToオプションでログインページにリダイレクト
+    await signOut({
+        redirectTo: "/login"
+    });
+}
